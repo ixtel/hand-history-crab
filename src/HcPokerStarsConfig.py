5276d25225424b5652577e1fb1551a40d6059023
jUrner <jUrner@arcor.de>
2011-10-12 05:19:18 +0000

    add PokerStarsConfig

---
+++ src/HcPokerStarsConfig.py
@@ -0,0 +1,17 @@
+
+import HcConfig
+
+#************************************************************************************
+#
+#************************************************************************************
+GameMapping = {
+		"Hold'em": HcConfig.GameHoldem,
+		}
+
+GameLimitMapping = {
+		"No Limit": HcConfig.GameLimitNoLimit,
+		}
+
+CurrencyMapping = {
+		'USD': HcConfig.CurrencyDollar,
+		}
